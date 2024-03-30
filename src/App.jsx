a7a32c0a8da09ead2680b6f639c93caf9adf2014
alwanjhs72@gmail.com <alwanjhs72@gmail.com>
2024-03-30 12:58:21 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navigation from './components/Navigation';
 import HomePagesWrapper from './pages/HomePageNotes';
 import AddPageNotes from './pages/AddPageNotes';
 import DetailPageWrapper from './pages/DetailPageNotes';
+import NotFoundNotes from './pages/NotFoundNotes';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       </header>
       <main>
         <Routes>
+          <Route path='*' element ={<NotFoundNotes />} />
           <Route path='/' element={<HomePagesWrapper />} />
           <Route path='/notes/new' element={<AddPageNotes />} />
           <Route path='/notes/:id' element={<DetailPageWrapper />} />
---
+++ src/pages/NotFoundNotes.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NotFoundNotes() {
+    return (
+        <div className='not-found'>
+            <h2>404</h2>
+            <h3>Sorry, Page Not Found</h3>
+        </div>
+    );
+}
+
+export default NotFoundNotes;
